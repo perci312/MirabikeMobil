513bd82ca1c13b83d94260a93f5d7b88e450da9d
perci312 <carloscstrns@gmail.com>
2023-09-27 21:44:49 -0300

    Revert "integracion del login"
    This reverts commit 6c88ef6dae3e216f098cd020299ec75668e75361.

--- MiraBike/src/app/app-routing.module.ts
+++ MiraBike/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AppComponent } from "./app.component";
-import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  { path: "", component: AppComponent, pathMatch: "full" },
-  { path: "login", component: LoginComponent, pathMatch: "full" },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+
+
 ];
 
 @NgModule({
--- MiraBike/src/app/app.module.ts
+++ MiraBike/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [AppComponent,LoginComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
