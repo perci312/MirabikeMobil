4fdeea5fb3a4dea768bf26853afafa8885a24bee
perci312 <carloscstrns@gmail.com>
2023-10-23 18:09:42 -0300

    BD-V2

--- MiraBike/src/app/pages/historial-ventas/historial-ventas.page.ts
+++ MiraBike/src/app/pages/historial-ventas/historial-ventas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-historial-ventas',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historial-ventas.page.scss'],
 })
 export class HistorialVentasPage implements OnInit {
+  datosVentas: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.obtenerDatosVentas();
   }
 
+  obtenerDatosVentas() {
+    const apiUrl = 'https://utgtbwb3r5.execute-api.us-east-2.amazonaws.com/venta';
+
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        const responseBody = JSON.parse(response.body);
+        if (Array.isArray(responseBody)) {
+          this.datosVentas = responseBody;
+        } else {
+          console.error('Error: Los datos no están en el formato esperado.');
+        }
+      },
+      (error) => {
+        console.error('Error al obtener datos de ventas:', error);
+      }
+    );
+  }
 }
--- MiraBike/src/app/pages/trabajos-taller/trabajos-taller.page.ts
+++ MiraBike/src/app/pages/trabajos-taller/trabajos-taller.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Asegúrate de importar HttpClient si lo necesitas
 
 @Component({
   selector: 'app-trabajos-taller',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trabajos-taller.page.scss'],
 })
 export class TrabajosTallerPage implements OnInit {
+  datosAppTaller: any[] = []; // Define la propiedad para almacenar los datos
 
-  constructor() { }
+  constructor(private http: HttpClient) {} // Asegúrate de inyectar HttpClient si lo necesitas
 
   ngOnInit() {
+    this.obtenerDatosAppTaller();
   }
 
+  obtenerDatosAppTaller() {
+    const apiUrl = 'https://de6yznumid.execute-api.us-east-2.amazonaws.com/taller'; // Reemplaza por la URL de tu API de taller
+
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        const responseBody = JSON.parse(response.body);
+        if (Array.isArray(responseBody)) {
+          this.datosAppTaller = responseBody; // Asigna los datos a la propiedad
+        } else {
+          console.error('Error: Los datos no están en el formato esperado.');
+        }
+      },
+      (error) => {
+        console.error('Error al obtener datos de taller:', error);
+      }
+    );
+  }
 }
+
