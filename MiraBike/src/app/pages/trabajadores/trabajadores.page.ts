5d2100734fbc229745c10bb41bc87c1410c5afe5
perci312 <carloscstrns@gmail.com>
2023-10-26 15:31:31 -0300

    v3

--- MiraBike/src/app/pages/trabajadores/trabajadores.page.ts
+++ MiraBike/src/app/pages/trabajadores/trabajadores.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-trabajadores',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trabajadores.page.scss'],
 })
 export class TrabajadoresPage implements OnInit {
+  datosAppPersona: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.obtenerDatosAppPersona();
   }
 
+  obtenerDatosAppPersona() {
+    const apiUrl = 'https://5o2imzmocc.execute-api.us-east-2.amazonaws.com/persona'; // Reemplaza con la URL de tu API de AWS Lambda
+
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        const responseBody = JSON.parse(response.body);
+        if (Array.isArray(responseBody)) {
+          this.datosAppPersona = responseBody;
+        } else {
+          console.error('Error: Los datos no están en el formato esperado.');
+        }
+      },
+      (error) => {
+        console.error('Error al obtener datos de app_persona:', error);
+      }
+    );
+  }
 }
+
