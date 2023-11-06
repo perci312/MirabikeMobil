55ab000fa3f534c43ac97791d1d89e1adb4024d8
perci312 <carloscstrns@gmail.com>
2023-11-05 23:13:37 -0300

    login v6

--- MiraBike/src/app/app.module.ts
+++ MiraBike/src/app/app.module.ts
@@ -8,14 +8,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CorsInterceptor } from './cors-interceptor';
-import { AuthGuard } from './auth.guard';
-import { MenuComponent } from './menu/menu.component';  
+
 @NgModule({
-  declarations: [AppComponent,MenuComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    AuthGuard 
+    
   ],
   bootstrap: [AppComponent],
 })
